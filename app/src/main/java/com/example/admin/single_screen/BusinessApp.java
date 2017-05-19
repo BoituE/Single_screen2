fc9d592433e44680d219ef2def28af29660e5a89
boituE <molisakengboitu@gmail.com>
2017-05-19 10:54:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/admin/single_screen/BusinessApp.java
@@ -0,0 +1,13 @@
+package com.example.admin.single_screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BusinessApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_app);
+    }
+}
